fc58968767808971ae9a4ce4837f8df5237a9b98
AeroSigma <ericdward@gmail.com>
2016-03-15 23:01:36 -0400

    tweaks

--- Mars2040Optim.m
+++ Mars2040Optim.m
@@ -1,21 +1,55 @@
 %% Architectures
-load(MorphArches.mat); %Now have Morph as a variable
-%need to figure out how to modify vars. Just i +/- 1 each time?  Search for
-%other Arches with all same except var of note?
-% alpha = Morph{i}; %Will be, once we figure out how to step through i
+% a_transCrew = {Crew.DRA_CREW, Crew.DEFAULT_TRANSIT},...don't use, do below
+% a_surfCrew = {SurfaceCrew.BIG_SURFACE, SurfaceCrew.MID_SURFACE, SurfaceCrew.MIN_SURFACE}, ... don't use this, use below?
 
-%could go this way, start with baseline, and alter vars independantly,
-%straightforward, but a _lot_ of copy/paste.
-% alpha = MarsArchitecture();
-% alpha.WHATEVER = SOMETHING;
+%the cell arrays define the possibilities of alpha
+%the values for a_vec define the current alpha
+%the values for a_vec.XXXXXmax set the max choice
+
+a_prop = {Propulsion.LH2, Propulsion.NTR}; 
+a_vec.prop = 1;
+a_vec.propmax = length(a_prop);
+
+a_returnFuel = {[ReturnFuel.EARTH_LH2, ReturnFuel.EARTH_O2],[ReturnFuel.EARTH_LH2,ReturnFuel.MARS_O2],[ReturnFuel.MARS_LH2,ReturnFuel.MARS_O2], [ReturnFuel.ASCENT_LH2, ReturnFuel.ASCENT_O2]};
+a_vec.returnFuel = 1;
+a_vec.returnFuelmax = length(a_returnFuel);
+
+a_surfPower = {PowerSource.NUCLEAR, PowerSource.SOLAR};
+a_vec.surfPower = 1;
+a_vec.surfPowermax = length(a_surfPower);
+
+a_entry = {ArrivalEntry.AEROCAPTURE, ArrivalEntry.PROPULSIVE};
+a_vec.entry = 1;
+a_vec.entrymax = length(a_entry);
+
+a_site = {Site.HOLDEN, Site.GALE, Site.MERIDIANI, Site.GUSEV, Site.ISIDIS, Site.ELYSIUM, Site.MAWRTH, Site.EBERSWALDE, Site.UTOPIA, Site.HELLAS, Site.AMAZONIS};
+a_vec.site = 1;
+a_vec.sitemax = length(a_site);
+
+a_food = {FoodSource.EARTH_MARS_50_SPLIT,FoodSource.MARS_ONLY,FoodSource.EARTH_MARS_25_75, FoodSource.EARTH_MARS_75_25};
+a_vec.food = 1;
+a_vec.foodmax = length(a_food);
+
+%this generates the current architecture based on the a_vec vector
+Cur_Arch = MarsArchitecture.Enumerate( ...
+    {Location.LEO}, ... 
+    {SurfaceCrew.MID_SURFACE}, ...
+    {SurfaceShielding.REGOLITH}, ...
+    {Crew.DRA_CREW}, ...
+    {[TransitFuel.EARTH_LH2,TransitFuel.LUNAR_O2]}, ...
+    {ArrivalCargoEntry.AEROCAPTURE}, ...
+    {a_prop{a_vec.prop}}, {a_returnFuel{a_vec.returnFuel}}, {a_surfPower{a_vec.surfPower}}, ...
+    {a_entry{a_vec.entry}}, {a_site{a_vec.site}}, {a_food{a_vec.food}} ...
+    );
 
 %% How to change propulsion numbers
-% TempProp = Cur_Arch.PropulsionType;
-% Cur_Arch.PropulsionType.Isp = 450; %seconds
-% TempProp.FuelOxRatio = 6; %shouldn't change
-% TempProp.IntertMassRatio = 0.17; %ratio (decimal percent)
-% TempProp.StaticMass = 0; %kg
-% Cur_Arch.PropulsionType = TempProp;
+%get the initial table:
+propulsionProps = readtable('propulsionTypesORIG.dat','ReadRowNames',true);
+%change a value, use prop type as the row name (LH2, NTR, CH4) and variable
+%as col. name (Isp, FuelOxidizerRatio, InertMassRatio, StaticMass):
+propulsionProps{'LH2','Isp'} = 480;
+%overwrite the .dat file, where the values will be taken from during simulation:
+writetable(propulsionProps,'./Classes/Arch_Classes/propulsionTypes.dat','WriteRowNames',true);
 
 %% How to change Surface Crew size
 % Cur_Arch.SurfaceCrew.Size = 32;
@@ -28,4 +62,4 @@ load(MorphArches.mat); %Now have Morph as a variable
 
 %% Run the Simulation
 
-[Results] = SingleTradeFunction(alpha, ISRU_e);
+% [Results] = SingleTradeFunction(Cur_Arch, ISRU_e);
